0a9ad9a3ef0b5d15db81b259c3edf52584c43fba
Ade Akinyede <t.akinyede@gmail.com>
2023-03-19 19:29:41 +0000

    [New]: FizzBuzz returns 'Fizz'.

---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export default function fizzBuzz(input: number): String {
+    return "Fizz"
+}
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,8 @@
+import fizzBuzz from './fizzbuzz'
 
 describe("fizzbuzz", () => {
-
+    it('should return the word "Fizz"', () => {
+        expect(fizzBuzz(3)).toBe('Fizz')
+    })
 
 });
