9a5224e843caf3966f17798df99ae2d7cd591c2c
Ade Akinyede <t.akinyede@gmail.com>
2023-03-20 21:40:38 +0000

    [Improvement]: AAdd estnumber range restriction of 1 to 100 for the input.

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,14 @@
-export default function fizzBuzz(input: number): String {
+// See https://stackoverflow.com/a/39495173 for solution used in restricting
+// the number range.
+type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>
+
+type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>
+
+type FizzBuzzRange = IntRange<1, 100>
+
+export default function fizzBuzz(input: FizzBuzzRange): String {
     if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz"
     else if (input % 3 === 0) return "Fizz"
     else if (input % 5 === 0) return "Buzz"
