9d3237aa949302bf4690834fc885ffa52e3b9f45
Ade Akinyede <t.akinyede@gmail.com>
2023-03-21 21:16:00 +0000

    [New]: Add initial isAPalindrome implemenation that returns true.

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,7 @@
+import { isAPalindrome } from './index'
 
 describe('palindrome checker', () => {
-
+    it('should detect that "mom" is a palindrome', () => {
+        expect(isAPalindrome('mom')).toBeTruthy()
+    })
 })
