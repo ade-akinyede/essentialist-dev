040329df93b64e7bf02ad4b6caca7b23a2077a50
Ade Akinyede <t.akinyede@gmail.com>
2023-03-23 19:17:56 +0000

    [New]: Add invalidation check for passwords less than 5 and more than 15 characters long.

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -3,10 +3,17 @@ import { PasswordValidator } from "./index"
 describe('password validator', () => {
     const passwordValidator = new PasswordValidator()
 
-    it('returns object with boolean result and errors key', () => {
-        expect(passwordValidator.validate("")).toStrictEqual({
+    it('invalidates password less than 5 characters long', () => {
+        expect(passwordValidator.validate("less")).toStrictEqual({
             result: false,
-            error: "Not yet implemented"
+            error: "Password must be between 5 and 15 characters long"
+        })
+    })
+    
+    it('invalidates password more than 15 characters long', () => {
+        expect(passwordValidator.validate("kDKSf823lkNmmOS-234")).toStrictEqual({
+            result: false,
+            error: "Password must be between 5 and 15 characters long"
         })
     })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -2,8 +2,15 @@ export class PasswordValidator {
     constructor() {}
 
     validate(password: string): {result: Boolean, error: String} {
+        const pwdLength = password.length
+        if (pwdLength < 5 || pwdLength > 15) {
+            return {
+                result: false,
+                error: 'Password must be between 5 and 15 characters long'
+            }
+        }
         return {
-            result: false,
+            result: true,
             error: 'Not yet implemented'
         }
     }
