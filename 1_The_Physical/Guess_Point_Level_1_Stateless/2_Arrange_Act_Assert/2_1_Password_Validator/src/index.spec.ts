ddedc843dbf6e829ae8af2be2daed8ce101ccf37
Ade Akinyede <t.akinyede@gmail.com>
2023-03-23 19:07:15 +0000

    [New]: Add PasswordValidator that returns object containing boolean result and an errors key.

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,13 @@
+import { PasswordValidator } from "./index"
 
 describe('password validator', () => {
+    const passwordValidator = new PasswordValidator()
 
+    it('returns object with boolean result and errors key', () => {
+        expect(passwordValidator.validate("")).toStrictEqual({
+            result: false,
+            error: "Not yet implemented"
+        })
+    })
 })
 
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -0,0 +1,10 @@
+export class PasswordValidator {
+    constructor() {}
+
+    validate(password: string): {result: Boolean, error: String} {
+        return {
+            result: false,
+            error: 'Not yet implemented'
+        }
+    }
+}
